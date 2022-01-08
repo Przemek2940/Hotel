c96acb1d54a410992d73b480070518e380e0490a
Przemek <przemek2940@gmail.com>
2022-01-08 16:52:34 +0100

    Zameldowanie gościa, obliczenie ceny, pozostawienie bagażu, obsłużenie pralni, pozostawienie auta, kosztprania naprawiony. Zapisywanie meldowania do pliku zrobione, data wymeldowania naprawiona, zapisywanie danych w linii poprawione. Zapisywanie wszystkich (oprócz pralni, bo bezsens) informacji do oddzielnych plików zrobione.
    Trzeba dodać wyrzucanie błędu, jeśli ktoś chce wpisać pokój więcej niż 4osobowy.

--- hotel.py
+++ hotel.py
@@ -11,34 +11,7 @@ pokoj3 = 42
 pokoj2 = 48
 pokoj1 = 60
 pranie = 7
-
-
-if decyzja == "1":
-    imie = input("Podaj imię i nazwisko: ")
-    paszport = input("Podaj numer paszportu: ")
-    dataur = input("Podaj datę urodzenia: ")
-    firma = input("Jaka firma?")
-    ileos = input("Iluosobowy pokój?")
-    pokoj = input("Numer pokoju: ")
-    czas = int(input("Ile dni?"))
-    data = date.today()
-    datawymeldowania = str(data + datetime.timedelta(days=czas))
-    dane = ['Imię i nazwisko: ', imie, 'Numer paszportu: ', paszport, 'Data urodzenia: ', dataur, 'Firma: ',
-            firma,'Numer pokoju: ', pokoj, 'Data zameldowania: ', str(data), 'Zostaje do: ', datawymeldowania, '\n']
-    with open('meldunki.txt', 'a') as f:
-        for linia in dane:
-            f.write(linia + '\n')
-elif decyzja == "2":
-    imie = input("Podaj imię i nazwisko: ")
-    ile = int(input("Ile bagaży? "))
-elif decyzja == "3":
-    ilepran = int(input("Ile prań? "))
-elif decyzja == "4":
-    rejestracja = input("Podaj numer rejestracjny: ")
-    imie = input("Podaj imię i nazwisko: ")
-else:
-    print("Błąd")
-
+data = date.today()
 
 def cena():
     if ileos == "4":
@@ -49,20 +22,54 @@ def cena():
         kwota = czas * pokoj2
     elif ileos == "1":
         kwota = czas * pokoj1
-        print("Błąd.")
     return kwota
 
 
-def main(args):
+if decyzja == "1":
+    imie = input("Podaj imię i nazwisko: ")
+    paszport = input("Podaj numer paszportu: ")
+    dataur = input("Podaj datę urodzenia: ")
+    firma = input("Jaka firma?")
+    ileos = input("Iluosobowy pokój?")
+    pokoj = input("Numer pokoju: ")
+    czas = int(input("Ile dni?"))
+    datawymeldowania = str(data + datetime.timedelta(days=czas))        #dodaje do daty zameldowania dni z czas
+    danemel = ['Imię i nazwisko: ' + imie, 'Numer paszportu: ' + paszport, 'Data urodzenia: ' + dataur, 'Firma: ' +
+            firma, 'Numer pokoju: ' + pokoj, 'Data zameldowania: ' + str(data), 'Zostaje do: ' + datawymeldowania,
+            'Kwota: ' + str(cena()) + 'zł', '\n']
+    with open('meldunki.txt', 'a') as f:
+        for linia in danemel:
+            f.write(linia + '\n')                                       #zapisywanie do pliku meldunki.txt
+elif decyzja == "2":
+    imie = input("Podaj imię i nazwisko: ")
+    ile = int(input("Ile bagaży? "))
+    danebag = ['Imię i nazwisko: ' + imie, 'Ilość bagaży: ' + str(ile), 'Kiedy zostawiono: ' + str(data), '\n']
+    with open('bagaze.txt', 'a') as f:
+        for linia in danebag:
+            f.write(linia + '\n')                                      #zapisywanie do pliku bagaze.txt
+elif decyzja == "3":
+    ilepran = int(input("Ile prań? "))                                  #nie ma potrzeby zapisywania, to tylko kalkulator
+elif decyzja == "4":
+    rejestracja = input("Podaj numer rejestracjny: ")
+    imie = input("Podaj imię i nazwisko: ")
+    danesam = ['Imię i nazwisko: ' + imie, "Numer rejestracyjny: ", rejestracja, 'Kiedy zostawiono: ' + str(data), '\n']
+    with open('samochod.txt', 'a') as f:
+        for linia in danesam:
+            f.write(linia + '\n')                                      #zapisywanie do pliku samochod.txt
+else:
+    print("Błąd")
+
+
+def main(args):                                                        #informacje pokazujące się w programie
     if decyzja == "1":
         print(imie, "musi zapłacić", cena(), "złotych za meldunek.")
     elif decyzja == "2":
         if ile == 1:
-            print(imie, "zostawił", ile, "bagaż, dnia:", date.today())
+            print(imie, "zostawił", ile, "bagaż, dnia:", data)
         elif ile > 1 and ile < 5:
-            print(imie, "zostawił", ile, "bagaże", date.today())
+            print(imie, "zostawił", ile, "bagaże", data)
         else:
-            print(imie, "zostawił", ile, "bagaży", date.today())
+            print(imie, "zostawił", ile, "bagaży", data)
     elif decyzja == "3":
         kosztprania = ilepran * pranie
         if ilepran == 1:
@@ -72,7 +79,7 @@ def main(args):
         else:
             print(ilepran, "prań kosztować będzie", kosztprania, "zł.")
     elif decyzja == "4":
-        print(imie, "zostawił auto o numerach rejestracyjnych: ", rejestracja, ". Dnia: ", date.today())
+        print(imie, "zostawił auto o numerach rejestracyjnych: ", rejestracja, ". Dnia: ", data)
     else:
         print("Błąd")
 
