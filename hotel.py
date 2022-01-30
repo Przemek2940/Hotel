d72d429ed39891fec22c23ccbf951ed686314da5
Przemek <przemek2940@gmail.com>
2022-01-30 14:16:12 +0100

    Zameldowanie gościa, obliczenie ceny, pozostawienie bagażu, obsłużenie pralni, pozostawienie auta, kosztprania naprawiony. Zapisywanie meldowania do pliku txt, data wymeldowania naprawiona, zapisywanie danych w linii poprawione. Zapisywanie wszystkich (oprócz pralni, bo bezsens) informacji do oddzielnych plików zrobione. Język angielski wdrożony, funkcje wrzucone na górę, możliwe wprowadzanie pokoi wraz z cenami do bazy danych, dodana informacja o obecnych pokojach w bazie danych, przeszukiwanie bazy danych w celu rozstrzygnięcia czy dany pokój istnieje czy trzeba stworzyć zrobione, liczenie ceny razy dni z pliku db zrobione, funkcja usuwania pokoi zrobiona, klasy wprowadzone
    Do zrobienia:
    printowanie ceny

--- hotel.py
+++ hotel.py
@@ -5,9 +5,105 @@ from datetime import date
 import datetime
 import sqlite3
 
+class Client:
+    def __init__(self, name = '', passport = '', birth = '', company = ''):
+        self.name = name
+        self.passport = passport
+        self.birth = birth
+        self.company = company
+
+    def inputuserdata(self):
+        self.name = input("Podaj imię i nazwisko: ")
+        self.passport = input("Podaj numer paszportu: ")
+        self.birth = input("Podaj datę urodzenia: ")
+        self.company = input("Jaka firma? ")
+
+
+
+class Room_management:
+    def roomdata(self):  # prints available rooms
+        con = sqlite3.connect('pokoje.db')
+        cur = con.cursor()
+        cur.execute("SELECT * FROM room")
+        results = cur.fetchall()
+        for row in results:
+            print("\nDostępne pokoje:")
+            print(row[0], "osobowy pokój")
+            print("Cena: ", row[1], "\n")
+
+    def roomcreator(self):
+        con = sqlite3.connect('pokoje.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS room (
+                beds INTEGER PRIMARY KEY ASC,
+                price varchar(250) DEFAULT ''
+            )""")
+
+        nbeds = input("Iluosobowy pokój chcesz stworzyć? ")
+        nprice = input("Jaka będzie cena za osobę? ")
+
+        cur.execute("""
+        INSERT OR IGNORE INTO room (beds, price)
+        VALUES (?,?)
+        """, (nbeds, nprice))
+        con.commit()
+
+    def roomdelete(self):
+        con = sqlite3.connect('pokoje.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        cur.execute(f"DELETE from room WHERE beds = '{roomtodelete}'")
+        con.commit()
+        exit()
+
+    def roomchecking(self):  # searching for room in database
+        con = sqlite3.connect('pokoje.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        if whichroom > 0:
+            cur.execute(f"SELECT * from room WHERE beds='{whichroom}'")
+            if cur.fetchall():
+                return whichroom
+            else:
+                creatordecision = input(
+                    "Nie ma takiego pokoju. Chcesz go stworzyć? (t/n)")  # posibility to create a room
+                if creatordecision == 't':
+                    x.roomcreator()
+                    x.roomdata()
+                    x.roomchecking()
+                else:
+                    print("Błąd1")
+        elif whichroom == 0:
+            x.roomcreator()
+            x.roomdata()
+            whichroom2 = int(input("Iluosobowy pokój chcesz wynająć? "))
+            cur.execute(f"SELECT * from room WHERE beds='{whichroom2}'")
+            if cur.fetchall():
+                return whichroom2
+            else:
+                print("Błąd2")
+
+    def amount(self):
+        con = sqlite3.connect('pokoje.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        if whichroom > 0:
+            cur.execute(f"SELECT price, beds FROM room WHERE beds = {whichroom}")
+            results = cur.fetchall()
+            for row in results:
+                print(int(row[0]) * time)
+        elif whichroom == 0:  # whichroom2 here too
+            cur.execute(f"SELECT price, beds FROM room WHERE beds = {whichroom2}")
+            results = cur.fetchall()
+            for row in results:
+                print(int(row[0]) * time)
+
+
 def main(args):                                                        #program screen
     if decision == "1":
-        print(name, "musi zapłacić", "tu powinna być funkcja " "złotych za meldunek.")
+        print(c.name, "musi zapłacić", r.amount(), "złotych za meldunek.")
     if decision == "2":
         if hmany == 1:
             print(name, "zostawił", hmany, "bagaż, dnia:", date)
@@ -25,89 +121,10 @@ def main(args):                                                        #program 
             print(hmanywashes, "prań kosztować będzie", washingcost, "zł.")
     elif decision == "4":
         print(name, "zostawił auto o numerach rejestracyjnych: ", registrationnumb, ". Dnia: ", date)
-    else:
-        print("Błąd")
 
 
-def roomdata():                                                   #prints available rooms
-    con = sqlite3.connect('pokoje.db')
-    cur = con.cursor()
-    cur.execute("SELECT * FROM room")
-    results = cur.fetchall()
-    for row in results:
-        print("\nDostępne pokoje:")
-        print(row[0], "osobowy pokój")
-        print("Cena: ", row[1], "\n")
 
 
-def roomcreator():
-    con = sqlite3.connect('pokoje.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS room (
-            beds INTEGER PRIMARY KEY ASC,
-            price varchar(250) DEFAULT ''
-        )""")
-
-    nbeds = input("Iluosobowy pokój chcesz stworzyć? ")
-    nprice = input("Jaka będzie cena za osobę? ")
-
-    cur.execute("""
-    INSERT OR IGNORE INTO room (beds, price)
-    VALUES (?,?)
-    """, (nbeds, nprice))
-    con.commit()
-
-
-def roomdelete():
-    con = sqlite3.connect('pokoje.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute(f"DELETE from room WHERE beds = '{roomtodelete}'")
-    con.commit()
-    exit()
-
-
-def roomchecking():                                     # searching for room in database
-    con = sqlite3.connect('pokoje.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    if whichroom > 0:
-        cur.execute(f"SELECT * from room WHERE beds='{whichroom}'")
-        if cur.fetchall():
-            return whichroom
-        else:
-            creatordecision = input("Nie ma takiego pokoju. Chcesz go stworzyć? (t/n)") #posibility to create a room
-            if creatordecision == 't':
-                roomcreator()
-                roomdata()
-                roomchecking()
-            else:
-                print("Błąd")
-    elif whichroom == 0:                                            # what to dooooooo
-        roomcreator()
-        roomdata()
-        cur.execute(f"SELECT * from room WHERE beds='{whichroom2}'")
-        if cur.fetchall():
-            return whichroom2
-        else:
-            print("Błąd")
-
-def amount():
-    con = sqlite3.connect('pokoje.db')
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    if whichroom > 0:
-        cur.execute(f"SELECT price, beds FROM room WHERE beds = {whichroom}")
-        results = cur.fetchall()
-        for row in results:
-            print(int(row[0]) * time)
-    elif whichroom == 0:                                                # whichroom2 here too
-        cur.execute(f"SELECT price, beds FROM room WHERE beds = {whichroom2}")
-        results = cur.fetchall()
-        for row in results:
-            print(int(row[0]) * time)
 
 
 
@@ -116,27 +133,25 @@ Jeśli chcesz rozliczyć pralnię: wybierz 3\nJeśli chcesz rozliczyć postój s
 Jeśli chcesz dodać lub usunąć pokój: wybierz 0\n""")
 washing = 7
 date = date.today()
+r = Room_management()
+c = Client()
 
 if decision == "1":
-    name = input("Podaj imię i nazwisko: ")
-    passport = input("Podaj numer paszportu: ")
-    birth = input("Podaj datę urodzenia: ")
-    company = input("Jaka firma? ")
-    roomdata()
+    c.inputuserdata()
+    r.roomdata()
     whichroom = int(input("Który pokój wybrać? \nJeśli chcesz stworzyć pokój: wybierz 0 \n"))
     if whichroom == 0:
-        roomchecking()
-        whichroom2 = int(input("Iluosobowy pokój chcesz stworzyć i wynająć? "))         #whichroom2 here too
+        r.roomchecking()
     room = input("Numer pokoju: ")
     time = int(input("Ile dni?"))
-    amount()
     checkout = str(date + datetime.timedelta(days=time))        #date of checking into + days from time
-    checkinto = ['Imię i nazwisko: ' + name, 'Numer paszportu: ' + passport, 'Data urodzenia: ' + birth, 'Firma: ' +
-            company, 'Numer pokoju: ' + room, 'Data zameldowania: ' + str(date), 'Zostaje do: ' + checkout,
-            'Kwota: ' + str(amount) + 'zł', '\n']
+    checkinto = ['Imię i nazwisko: ' + c.name, 'Numer paszportu: ' + c.passport,
+                'Data urodzenia: ' + c.birth, 'Firma: ' + c.company, 'Numer pokoju: ' + room,
+                'Data zameldowania: ' + str(date), 'Zostaje do: ' + checkout, 'Kwota: ' + str(r.amount) + 'zł', '\n']
     with open('meldunki.txt', 'a') as f:
         for linia in checkinto:
-            f.write(linia + '\n')                                       #saving to meldunki.txt
+            f.write(linia + '\n')
+
 elif decision == "2":
     name = input("Podaj imię i nazwisko: ")
     hmany = int(input("Ile bagaży? "))
@@ -156,13 +171,13 @@ elif decision == "4":
 elif decision == "0":                          # if room is not in database - roomcreator
     createordelete = input("Jeśli chcesz stworzyć pokój, wyślij: s\nJeśli chcesz usunąć pokój wyślij: u")
     if createordelete == 's':
-        roomcreator()
+        r.roomcreator()
     elif createordelete == 'u':
-        roomdata()
+        r.roomdata()
         roomtodelete = int(input("Iluosobowy pokój chcesz usunąć?"))
-        roomdelete()
+        r.roomdelete()
 else:
-    print("Błąd")
+    print("Błąd4")
 
 if __name__ == '__main__':
     import sys
