12d4986757b14e5c11c621bb38362dab4a64a0b9
Przemek <przemek2940@gmail.com>
2022-02-19 13:02:34 +0100

    Zameldowanie gościa, obliczenie ceny, pozostawienie bagażu, obsłużenie pralni, pozostawienie auta. Zapisywanie meldowania do pliku txt, zapisywanie danych w linii; zapisywanie wszystkich (oprócz pralni, bo bezsens) informacji do oddzielnych plików zrobione; możliwe wprowadzanie pokoi wraz z cenami do bazy danych, dodana informacja o obecnych pokojach w bazie danych, przeszukiwanie bazy danych w celu rozstrzygnięcia czy dany pokój istnieje czy trzeba stworzyć; liczenie ceny razy dni z pliku db zrobione, funkcja usuwania pokoi zrobiona, klasy wprowadzone, printowanie ceny zrobione, formatowanie stringów zrobione, klasa Client przeniesiona do osobnego pliku, stworzenie pliku do prania zrobione, klasa bagaże zrobiona, pętla żeby można było dalej zostać w programie zrobiona,
    Do zrobienia:
    Obsługa bazy danych bagaży - unique constraint - trzeba pomyśleć co zrobić z pierwszą kolumną, na razie ustawione na IGNORE; obsługa "nie ma takiego pokoju, chcesz stworzyć?"; pętle do dodawania/usuwania plików,baza danych klientów; przeniesienie klasy RoomManagement do osobnego pliku [problem z importowaniem cyrkularnym],

--- hotel.py
+++ hotel.py
@@ -90,7 +90,7 @@ class RoomManagement:
                 return int(row[0]) * time
 
 
-def main(args):                                                        # program screen
+def main():                                                        # program screen
     if decision == "1":
         print("%s musi zapłacić %s złotych za meldunek" % (c.name, r.amount()))
     if decision == "2":
@@ -112,71 +112,78 @@ def main(args):                                                        # program
         print("%s zostawił auto o numerach rejestracyjnych: %s. Dnia: %s" % (name, registrationnumb, date))
 
 
-decision = input("""Meldowanie: wybierz 1.\nBagaże: wybierz 2\nPralnia: wybierz 3
-Postój samochodu: wybierz 4\nJeśli chcesz dodać lub usunąć pokój: wybierz 0\n""")
 washing = 7
 date = date.today()
 timehr = datetime.datetime.now()
 r = RoomManagement()
 
-if decision == "1":
-    c.inputuserdata()
-    r.roomdata()
-    whichroom = int(input("Który pokój wybrać? \nJeśli chcesz stworzyć pokój: wybierz 0 \n"))
-    if whichroom == 0:
-        whichroom2 = int(input("Iluosobowy pokój chcesz stworzyć? "))
-        nprice = input("Jaka będzie cena za osobę? ")
-        nbeds = whichroom2
-        r.roomchecking()
-        print("Wybrany pokój: %s-osobowy" % whichroom2)
+while True:
+    print("\n" + "x" * 40 + "\n")
+    decision = input("""Meldowanie: wybierz 1.\nBagaże: wybierz 2\nPralnia: wybierz 3
+Postój samochodu: wybierz 4\nJeśli chcesz dodać lub usunąć pokój: wybierz 0\nJeśli chcesz zakonczyć: wybierz x\n""")
+
+    if decision == "1":
+        c.inputuserdata()
+        r.roomdata()
+        whichroom = int(input("Który pokój wybrać? \nJeśli chcesz stworzyć pokój: wybierz 0 \n"))
+        if whichroom == 0:
+            whichroom2 = int(input("Iluosobowy pokój chcesz stworzyć? "))
+            nprice = input("Jaka będzie cena za osobę? ")
+            nbeds = whichroom2
+            r.roomchecking()
+            print("Wybrany pokój: %s-osobowy" % whichroom2)
+        else:
+            r.roomchecking()
+        room = input("Numer pokoju: ")
+        time = int(input("Ile dni?"))
+        checkout = str(date + datetime.timedelta(days=time))        # date of checking into + days from time
+        checkinto = ['Imię i nazwisko: %s \nNumer paszportu: %s \nData urodzenia: %s \nFirma: %s \nNumer pokoju: %s' 
+                     '\nData zameldowania: %s \nZostaje do: %s \nKwota: %szł\n' % (c.name, c.passport, c.birth,
+                                                                                   c.company, room, str(date), checkout,
+                                                                                   str(r.amount()))]
+
+        with open('meldunki.txt', 'a') as f:
+            for linia in checkinto:
+                f.write(linia + '\n')
+
+    elif decision == "2":
+        whattodo = input("Dodanie bagażu: Wybierz 1\nOdebranie bagażu: Wybierz 2\n")
+        if whattodo == '1':                                         # luggage class teleport
+            l.inputluggage()
+            l.txtsaving()
+            l.dbsaving()
+    elif decision == "3":
+        hmanywashes = int(input("Ile prań? "))
+        roomnumber = input("Numer pokoju: ")
+        washes = ['Data i godzina prania: %s:%s, %s, Pokój: %s' % (timehr.hour, timehr.minute, date, roomnumber)]
+        with open('pranie.txt', 'a') as f:
+            for linia in washes:
+                f.write(linia + '\n')
+    elif decision == "4":
+        registrationnumb = input("Podaj numer rejestracjny: ")
+        name = input("Podaj imię i nazwisko: ")
+        car = ['Imię i nazwisko: %s\nNumer rejestracyjny: %s\nZostawiono dnia: %s\n' % (name, registrationnumb,
+                                                                                        str(date))]
+        with open('samochod.txt', 'a') as f:
+            for linia in car:
+                f.write(linia + '\n')                                      # saving to samochod.txt
+    elif decision == "0":                          # if room is not in database - roomcreator
+        createordelete = input("Jeśli chcesz stworzyć pokój, wyślij: s\nJeśli chcesz usunąć pokój wyślij: u\n")
+        if createordelete == 's':
+            nbeds = input("Iluosobowy pokój chcesz stworzyć?")
+            nprice = input("Jaka będzie cena za osobę? ")
+            r.roomcreator()
+            print("Nowy stan pokoi:")
+            r.roomdata()
+        elif createordelete == 'u':
+            r.roomdata()
+            roomtodelete = int(input("Iluosobowy pokój chcesz usunąć?"))
+            r.roomdelete()
+    elif decision == "x":
+        break
     else:
-        r.roomchecking()
-    room = input("Numer pokoju: ")
-    time = int(input("Ile dni?"))
-    checkout = str(date + datetime.timedelta(days=time))        # date of checking into + days from time
-    checkinto = ['Imię i nazwisko: %s \nNumer paszportu: %s \nData urodzenia: %s \nFirma: %s \nNumer pokoju: %s' 
-                 '\nData zameldowania: %s \nZostaje do: %s \nKwota: %szł\n' % (c.name, c.passport, c.birth, c.company,
-                                                                            room, str(date), checkout, str(r.amount()))]
+        print("Błąd4")
 
-    with open('meldunki.txt', 'a') as f:
-        for linia in checkinto:
-            f.write(linia + '\n')
-
-elif decision == "2":
-    whattodo = input("Dodanie bagażu: Wybierz 1\nOdebranie bagażu: Wybierz 2\n")
-    if whattodo == '1':                                         # luggage class teleport
-        l.inputluggage()
-        l.txtsaving()
-        l.dbsaving()
-elif decision == "3":
-    hmanywashes = int(input("Ile prań? "))
-    roomnumber = input("Numer pokoju: ")
-    washes = ['Data i godzina prania: %s:%s, %s, Pokój: %s' % (timehr.hour, timehr.minute, date, roomnumber)]
-    with open('pranie.txt', 'a') as f:
-        for linia in washes:
-            f.write(linia + '\n')
-elif decision == "4":
-    registrationnumb = input("Podaj numer rejestracjny: ")
-    name = input("Podaj imię i nazwisko: ")
-    car = ['Imię i nazwisko: %s\nNumer rejestracyjny: %s\nZostawiono dnia: %s\n' % (name, registrationnumb, str(date))]
-    with open('samochod.txt', 'a') as f:
-        for linia in car:
-            f.write(linia + '\n')                                      # saving to samochod.txt
-elif decision == "0":                          # if room is not in database - roomcreator
-    createordelete = input("Jeśli chcesz stworzyć pokój, wyślij: s\nJeśli chcesz usunąć pokój wyślij: u\n")
-    if createordelete == 's':
-        nbeds = input("Iluosobowy pokój chcesz stworzyć?")
-        nprice = input("Jaka będzie cena za osobę? ")
-        r.roomcreator()
-        print("Nowy stan pokoi:")
-        r.roomdata()
-    elif createordelete == 'u':
-        r.roomdata()
-        roomtodelete = int(input("Iluosobowy pokój chcesz usunąć?"))
-        r.roomdelete()
-else:
-    print("Błąd4")
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+    if __name__ == '__main__':
+        main()
+exit()
